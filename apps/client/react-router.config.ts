6b4617df1b4627f613165fbb7fff081944b5f993
Abdul Samad Ansari <hello@abdulsamad.dev>
2025-03-13 06:48:47 +0530

    refactor: upgraded to vite 6

--- apps/client/react-router.config.ts
+++ apps/client/react-router.config.ts
@@ -2,6 +2,6 @@ import { vercelPreset } from '@vercel/react-router/vite';
 import type { Config } from '@react-router/dev/config';
 
 export default {
-  ssr: false, // Server-side render by default, to enable SPA mode set this to `false`
+  ssr: false, // Server-side render by default
   presets: [vercelPreset()],
 } satisfies Config;
